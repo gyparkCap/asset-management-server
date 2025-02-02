1929914c9dc21174e512589252b2afce20989973
gyparkCap <gy.park.cap@gmail.com>
2025-02-02 16:50:13 +0900

    first commit

---
+++ src/main/kotlin/gypark/assetManagement/AssetManagementApplication.kt
@@ -0,0 +1,11 @@
+package gypark.assetManagement
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AssetManagementApplication
+
+fun main(args: Array<String>) {
+	runApplication<AssetManagementApplication>(*args)
+}
---
+++ src/test/kotlin/gypark/assetManagement/AssetManagementApplicationTests.kt
@@ -0,0 +1,13 @@
+package gypark.assetManagement
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AssetManagementApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
